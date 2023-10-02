d1201e1aa8a7198690d38280a422eaf502c6cd3f
Gil Sousa <tiagosousafl@gmail.com>
2023-10-02 17:03:04 +0100

    [feat] add product to cart

--- src/cart.py
+++ src/cart.py
@@ -2,11 +2,17 @@ from product import Product
 
 
 class Cart:
-    def __init__(self):
+    def __init__(self) -> None:
         self.items = {}
 
-    def add_product(self, product: Product):
-        raise
+    def add_product(self, product: Product, qtd: int = 1) -> None:
+        if self.items.get(product.code, None) is None:
+            self.items[product.code] = {
+                "price": product.price,
+                "qtd": qtd
+            }
+        else:
+            self.items[product.code]["qtd"] += qtd
 
     def remove_product(self, product: Product):
         raise
